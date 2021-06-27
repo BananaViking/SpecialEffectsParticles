659405ece9865b01ee9c9f2e1c6dbdeca1c5f501
Banana Viking <bananavikingcoder@gmail.com>
2021-06-27 10:43:23 -0500

    Add ParticleState and position function

--- Particles/ContentView.swift
+++ Particles/ContentView.swift
@@ -10,21 +10,72 @@ import SwiftUI
 
 struct EmitterView: View {
     private struct ParticleView: View {
+        @State private var isActive = false
+        let position: ParticleState<CGPoint>
+        
         var body: some View {
             Image("spark")
+                .position(isActive ? position.end : position.start)
+        }
+    }
+    
+    private struct ParticleState<T> {
+        var start: T
+        var end: T
+        
+        init(_ start: T, _ end: T) {
+            self.start = start
+            self.end = end
         }
     }
     
     var particleCount: Int
     
+    var creationPoint = UnitPoint.center
+    var creationRange = CGSize.zero
+    
+    var angle = Angle.zero
+    var angleRange = Angle.zero
+    
+    var speed = 50.0
+    var speedRange = 0.0
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
                 ForEach(0..<self.particleCount, id: \.self) { i in
-                    ParticleView()
+                    ParticleView(
+                        position: self.position(in: geo)
+                    )
                 }
             }
         }
+    }
+    
+    private func position(in proxy: GeometryProxy) -> ParticleState<CGPoint> {
+        let halfCreationRangeWidth = creationRange.width / 2
+        let halfCreationRangeHeight = creationRange.height / 2
+        
+        let creationOffsetX = CGFloat.random(in: -halfCreationRangeWidth...halfCreationRangeWidth)
+        let creationOffsetY = CGFloat.random(in: -halfCreationRangeHeight...halfCreationRangeHeight)
+        
+        let startX = Double(proxy.size.width * (creationPoint.x + creationOffsetX))
+        let startY = Double(proxy.size.height * (creationPoint.y + creationOffsetY))
+        let start = CGPoint(x: startX, y: startY)
+        
+        let halfSpeedRange = speedRange / 2
+        let actualSpeed = speed + Double.random(in: -halfSpeedRange...halfSpeedRange)
+        
+        let halfAngleRange = angleRange.radians / 2
+        let actualDirection = angle.radians + Double.random(in: -halfAngleRange...halfAngleRange)
+        
+        // - .pi / 2 subtracts 90 degrees from the direction since zero points right in SwiftUI
+        // but we think of zero as up
+        let finalX = cos(actualDirection - .pi / 2) * actualSpeed
+        let finalY = sin(actualDirection - .pi / 2) * actualSpeed
+        let end = CGPoint(x: startX + finalX, y: startY + finalY)
+        
+        return ParticleState(start, end)
     }
 }
 
